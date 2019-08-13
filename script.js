741bfb205427ece78a67389c0ee8e78598c87bba
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-13 11:29:43 -0600

    fixed api

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ $(document).on("click", ".options", function () {
     console.log(searchTerm);
 
     //inserts the keyword to be searched into the url, and set the limit of GIPHs returned to 10
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10"
 
     //ajax call
     $.ajax({
